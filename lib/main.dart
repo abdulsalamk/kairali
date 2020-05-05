ce357bc0d9d284965fabc814da90f92cb91f249e
Abdul Kunnummal <salam@okdok.co>
2020-05-05 10:09:23 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+  
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      routes: {
+        "/": (_) => new WebviewScaffold(
+          url: "https://britishkairali.co.uk/",
+          appBar: new AppBar(
+            title: new Text("British Kairali"),
+          ),
+        ),
+      },
+    );
+  }
+}
